9e3e8115cbb3e4688f17cffe5a0ad60596613ebb
Анастасия <113115043+psinaa@users.noreply.github.com>
2023-11-05 12:56:05 +0300

    Добавьте файлы проекта.

---
+++ HelloApp6/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace HelloApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox15 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox16 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox2.Location = new System.Drawing.Point(13, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(393, 35);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Добавить элемент в коллекцию";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox3.Location = new System.Drawing.Point(467, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(393, 35);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Удалить элемент из коллекции";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(467, 72);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(250, 26);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox5.Location = new System.Drawing.Point(897, 13);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(277, 35);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "Очистить коллекцию?";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(897, 72);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(57, 26);
+            this.textBox6.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1449, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 74);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Задание1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox7.Location = new System.Drawing.Point(1233, 13);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(177, 35);
+            this.textBox7.TabIndex = 7;
+            this.textBox7.Text = "Коллекция";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1233, 72);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(177, 26);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox9.Location = new System.Drawing.Point(12, 148);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(184, 35);
+            this.textBox9.TabIndex = 9;
+            this.textBox9.Text = "Имя человека";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(12, 208);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(184, 26);
+            this.textBox10.TabIndex = 10;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox11.Location = new System.Drawing.Point(244, 148);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(250, 35);
+            this.textBox11.TabIndex = 11;
+            this.textBox11.Text = "Возраст человека";
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(244, 208);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(250, 26);
+            this.textBox12.TabIndex = 12;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox13.Location = new System.Drawing.Point(527, 148);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(190, 35);
+            this.textBox13.TabIndex = 13;
+            this.textBox13.Text = "Адрес человека";
+            // 
+            // textBox14
+            // 
+            this.textBox14.Location = new System.Drawing.Point(527, 208);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(190, 26);
+            this.textBox14.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1449, 160);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 74);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Задание2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox15
+            // 
+            this.textBox15.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.textBox15.Location = new System.Drawing.Point(750, 184);
+            this.textBox15.Name = "textBox15";
+            this.textBox15.Size = new System.Drawing.Size(647, 26);
+            this.textBox15.TabIndex = 16;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1449, 331);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(317, 74);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Задание3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox16
+            // 
+            this.textBox16.Location = new System.Drawing.Point(763, 379);
+            this.textBox16.Name = "textBox16";
+            this.textBox16.Size = new System.Drawing.Size(647, 26);
+            this.textBox16.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1768, 743);
+            this.Controls.Add(this.textBox16);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox15);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox16;
+    }
+}
+
---
+++ HelloApp6/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace HelloApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s1 = textBox1.Text;
+            string s2 = textBox4.Text;
+            string s3 = textBox6.Text;
+
+            int a1, a2, a3;
+
+            if (int.TryParse(s1, out a1))
+            {
+                MyCollection<int> myIntCollection = new MyCollection<int>();
+                myIntCollection.Add(1);
+                myIntCollection.Add(2);
+                myIntCollection.Add(3);
+                myIntCollection.Add(4);
+                if (a1 != 0) myIntCollection.Add(a1);
+                if (int.TryParse(s2, out a2) && a2 != 0) myIntCollection.Remove(a2);
+                if (int.TryParse(s3, out a3) && a3 != 0) myIntCollection.Clear();
+                textBox8.Text = string.Join(", ", myIntCollection.ToArray());
+            }
+
+        }
+
+        public class MyCollection<T>
+        {
+            private List<T> items;
+
+            public MyCollection()
+            {
+                items = new List<T>();
+            }
+
+            public void Add(T item)
+            {
+                items.Add(item);
+            }
+
+            public void Clear()
+            {
+                items.Clear();
+            }
+
+            public void Remove(T item)
+            {
+                items.Remove(item);
+            }
+
+            public int Count
+            {
+                get
+                {
+                    return items.Count;
+                }
+            }
+
+            public T[] ToArray()
+            {
+                return items.ToArray();
+            }
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<Person> people = new List<Person>();
+            string s11 = textBox10.Text;
+            string s12 = textBox12.Text;
+            string s13 = textBox14.Text;
+            people.Add(new Person(s11, s12, s13));
+            string peopleInfo = "";
+            foreach (Person person in people)
+            {
+                peopleInfo += $"Имя: {person.Name}, Возраст: {person.Age}, Адрес: {person.Address}\r\n";
+            }
+
+            textBox15.Text = peopleInfo;
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public string Age { get; set; }
+            public string Address { get; set; }
+
+            public Person(string name, string age, string address)
+            {
+                Name = name;
+                Age = age;
+                Address = address;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string s11 = textBox10.Text;
+            string s12 = textBox12.Text;
+            string s13 = textBox14.Text;
+            Person newPerson = new Person(s11, s12, s13);
+            Dictionary<string, string> personInfo = GetPersonInfoDictionary(newPerson);
+            textBox16.Clear();
+            foreach (var kvp in personInfo)
+            {
+                textBox16.AppendText($"{kvp.Key}: {kvp.Value}\r\n");
+            }
+        }
+        private Dictionary<string, string> GetPersonInfoDictionary(Person person)
+        {
+            Dictionary<string, string> personInfo = new Dictionary<string, string>
+    {
+        { "Имя", person.Name },
+        { "Возраст", person.Age.ToString() },
+        { "Адрес", person.Address }
+    };
+            return personInfo;
+        }
+    }
+}
